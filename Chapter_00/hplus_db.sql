b81bd6b2a12602405f246482a558ba1a649d931f
Michael Rodgers <michael.d.rodgers.jr@gmail.com>
2019-02-20 06:04:30 -0800

    added new directory containing notes on installing brew and chocolatey as well as mysql

---
+++ Chapter_00/hplus_db.sql
@@ -0,0 +1,13 @@
+create database hplus_db;
+use hplus_db;
+create user 'hplus_user'@'localhost' IDENTIFIED BY 'My$3cret';
+grant select on hplus_db.* to 'hplus_user'@'localhost';
+grant insert on hplus_db.* to 'hplus_user'@'localhost';
+grant update on hplus_db.* to 'hplus_user'@'localhost';
+grant delete on hplus_db.* to 'hplus_user'@'localhost';
+flush privileges;
+
+create table products (id smallint unsigned not null auto_increment, product_name varchar(20) not null, constraint pk_example primary key (id) );
+insert into products ( id, product_name ) VALUES ( null, 'Fruity Water' );
+insert into products ( id, product_name ) VALUES ( null, 'Juicy Water' );
+insert into products ( id, product_name ) VALUES ( null, 'Wet Water' );
